e071070bb11ebfb9e0fad1647c79d28906aeb251
Feror <contact@feror.fr>
2024-09-24 16:29:18 +0200

    add et divide sont maintenant statiques, tests modifiés en conséquences

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -5,20 +5,13 @@ import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
-    Calculator calculator;
-
-    @BeforeEach
-    void setUp() {
-        calculator = new Calculator();
-    }
-
     @org.junit.jupiter.api.Test
     void add() {
-        assertEquals(5, calculator.add(2, 3));
+        assertEquals(5, Calculator.add(2, 3));
     }
 
     @org.junit.jupiter.api.Test
     void divide() {
-        assertEquals(2, calculator.divide(6, 3));
+        assertEquals(2, Calculator.divide(6, 3));
     }
 }
